8726bc093332a7e897d8e022db5cc3c7ec9fd798
supercoco-nft <99922407+supercoco-nft@users.noreply.github.com>
2022-02-19 17:20:07 +0100

    Add files via upload

--- waddles_bot.py
+++ waddles_bot.py
@@ -3,10 +3,12 @@ import discord
 from dotenv import load_dotenv
 import random
 import csv
+from discord.ext import commands#.commands import has_permissions, MissingPermissions
+#from discord import Member
 
 load_dotenv()
-TOKEN = os.getenv('bot_token')
-GUILD = os.getenv('bot_guild')
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
 
 bot_name = ""
@@ -33,58 +35,130 @@ all_chars = ['a', 'A', 'b', 'B', 'c', 'C', 'd', 'D', 'e', 'E', 'f', 'F',
 
 fields = ["Name", "Code"]  ## I could add time
 
-client = discord.Client()
+
+#client = discord.Client()
+client = commands.Bot(command_prefix ='.')
+
+#@client.command(name="kick", pass_context=True)
+#@has_permissions(manage_roles=True, ban_members=True)
+#async def _kick(ctx, member: Member):
+#    await bot.kick(member)#
+
+#@_kick.error
+#async def kick_error(ctx, error):
+#    if isinstance(error, MissingPermissions):
+#        text = "Sorry {}, you do not have permissions to do that!".format(ctx.message.author)
+#        await bot.send_message(ctx.message.channel, text)
+
+@client.command()
+async def ping(ctx):
+    print("biem")
+    await ctx.send("pong")
+
+
 
 @client.event
 async def on_ready():
     for guild in client.guilds:
         if guild.name == GUILD:
             break
+
+    guild = client.get_guild(guild.id)
+    for x in guild.members:
+        print(x)
     print(f'{client.user} has connected to Discord!')
 
     print(
         f'{client.user} is connected to the following guild:\n'
         f'{guild.name}(id: {guild.id})'
     )
-
     bot_name = str(client.user)
+
+    ##perms = discord.Permissions(send_messages=False, read_messages=True)
+    ##await client.create_role(server, name='NoSend', permissions=perms)
+
+    
     #invite_channel = await guild.create_text_channel(full_invite_name)
     #join_channel = await guild.create_text_channel(full_join_name)
     #check_channel = await guild.create_text_channel(full_check_name)
+    
+    #print(channel)
+    #print(discord.Permissions(5))
 
+
+    
 @client.event
 async def on_message(message):
     global all_chars
 
     channel = message.channel
-    #print(message.author)
+    print(message.author)
+    #member = message.author
+
+    #var = discord.utils.get(message.guild.roles, name = "new_role")
+    #member.add_role(var)
+
+    #server = ctx.message.server
+    #perms = discord.Permissions(send_messages=False, read_messages=True)
+    #await client.create_role(server, name='NoSend', permissions=perms)
+
+
+    
+    if message.author.guild_permissions.administrator:
+        print("the user is admin")
+    else:
+        print(message.author.guild_permissions.administrator)
+
 
     if str(message.author) != str(client.user):##name of the bot
         if str(channel) == full_invite_name:
+            print("biem")
             if message.content == "/create":
-                random_code = "SC-"
+                ##checks if the code is already generated
+                f = open("generated_codes.csv", "r")
+                reader = csv.reader(f)
+                organized_codes = list(reader)
+                f.close()
+                organized_codes = [ele for ele in organized_codes if ele != []]
 
-                while True:
-                    for _ in range(10):
-                        a = random.randint(0, len(all_chars)-1)
-                        random_code += str(all_chars[a])
 
-                    print(random_code)
-
-                    codes = []
-                    f = open("generated_codes.csv", "r")
-                    reader = csv.reader(f)
-                    for x in reader:
-                        for y in x:
-                            codes.append(y)
-                    if random_code not in codes:
+                code_created = False
+                for i in range(len(organized_codes)):
+                    print(organized_codes[i][0])
+                    if str(organized_codes[i][0]) == str(message.author):
+                        code_created = True
+                        generated_code = organized_codes[i][1]
                         break
                     
-                f = open("generated_codes.csv", "a")
-                csvwriter = csv.writer(f) 
-                csvwriter.writerow([str(message.author), random_code]) 
-                f.close()
-                await channel.send(f"Your invite code is {random_code}")
+
+                if code_created == False:
+                    random_code = "SC-"
+
+                    while True:
+                        for _ in range(10):
+                            a = random.randint(0, len(all_chars)-1)
+                            random_code += str(all_chars[a])
+
+                        print(random_code)
+
+                        codes = []
+                        f = open("generated_codes.csv", "r")
+                        reader = csv.reader(f)
+                        for x in reader:
+                            for y in x:
+                                codes.append(y)
+                        if random_code not in codes:
+                            break
+                        
+                    f = open("generated_codes.csv", "a")
+                    csvwriter = csv.writer(f) 
+                    csvwriter.writerow([str(message.author), random_code]) 
+                    f.close()
+                    
+                    await channel.send(f"Your invite code is {random_code}")
+
+                elif code_created == True:
+                    await channel.send(f"Your already generated code is {generated_code}")
 
         elif str(channel) == full_join_name:
             codes = []
@@ -98,10 +172,11 @@ async def on_message(message):
             organized_codes = [ele for ele in organized_codes if ele != []]
             code_found = False
 
+
             for i in range(len(organized_codes)):
                 if str(organized_codes[i][1]) == message.content:
                     code_found = True
-
+                    
                     creator = organized_codes[i][0]
                     ##gives an extra point to the creator
                     f  = open("score_tracker.csv", "r")
@@ -121,11 +196,22 @@ async def on_message(message):
                     if inlist == True:
                         current_score = int(organized_scores[place][1])
                         current_score += 1
+                        ##checks if the score is >= 10
+                        if organized_scores[place][2] == "0" and current_score >= 10:
+                            organized_scores[place][2] = "1"
+                            ##adds the creator to the "Member" role
+                            #s = message.Guild
+                            #member = s.get_member_named(creator)
+                            #role = discord.utils.get(member.guild.roles, name="Member")
+                            #await member.add_roles(role)
+                            print("added the creator to the 'Member' role")
+                            
                         current_score = str(current_score)
                         organized_scores[place][1] = current_score
 
                     else:
-                        organized_scores.append([creator, "1"])
+                                                ##name, score, member(0 == False)
+                        organized_scores.append([creator, "1", "0"])
 
                     f  = open("score_tracker.csv", "w")
                     writer = csv.writer(f)
@@ -134,7 +220,7 @@ async def on_message(message):
                         
                         
                     
-                    del organized_codes[i]
+                    #del organized_codes[i]
 
                     f  = open("generated_codes.csv", "w")
                     writer = csv.writer(f)
@@ -144,15 +230,15 @@ async def on_message(message):
                     break
 
             if code_found == True:
-                print("you're in!!!")
                 await channel.send("[+] You're in!!!")
 
-                     
+                
+                
             else:
                 await channel.send("[-] Code is not valid")
 
         elif str(channel) == full_check_name:
-            if (message.content).lower() == "check invites":
+            if (message.content).lower() == "/invites":
                 print("checking invites")
 
                 f = open("score_tracker.csv", "r")
@@ -161,18 +247,16 @@ async def on_message(message):
                 f.close()
                 organized_scores = [ele for ele in organized_scores if ele != []]
 
-                invited = False
                 for i in range(len(organized_scores)):
                     if organized_scores[i][0] == str(message.author):
+                        print(organized_scores[i][1])
                         await channel.send(f"You have currently invited {organized_scores[i][1]} qualified users")
-                        invited = True
+                        if organized_scores[i][2] == "1":
+                            await channel.send(f"So You are a BE MEMBER")
                         break
-
-                if invited == False:
-                    await channel.send(f"Sorry, I couldn't find any qualified users :(")
-
-            
-
+               
+#async def discord.VerificationLevel():         
+        
 
 
 
